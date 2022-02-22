7b54adfaa47fd98d90eac288f4e1f51e230d647e
Valera <artemwot90@gmail.com>
2022-02-22 18:56:31 +0200

    Sprint05

---
+++ Sprint05/t03_first_upper/index.js
@@ -0,0 +1,16 @@
+function firstUpper(string)
+{
+    if(string == null)
+    {
+        return "";
+    }
+    string = string.trim();
+    string = string.toLowerCase();
+    if((/[a-zA-Z]/).test(string[0]) == true)
+    {
+        string = string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    return string;
+}
+
+module.exports.firstUpper = firstUpper;
---
+++ Sprint05/t06_access/access.js
@@ -0,0 +1,17 @@
+module.exports =  class Access
+{
+    constructor(mark_LXXXV)
+    {
+        if(!mark_LXXXV)
+        {
+            return (this.mark_LXXXV = mark_LXXXV);
+        }
+        if(mark_LXXXV == null)
+        {
+            return (this.mark_LXXXV = mark_LXXXV);
+        }
+        return (this.mark_LXXXV = mark_LXXXV);
+    }
+};
+
+ 
